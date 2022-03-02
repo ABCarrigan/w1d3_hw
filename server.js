1cbd6a746a310ec40bc61465fb87c0640d9c8d9d
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-02 15:13:07 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/greeting/:name', (req, res) => {
-    res.send('Hello, ' + req.params.name);
-  });
+  res.send('Hello, ' + req.params.name);
+});
 
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+  let tipPercentile = req.params.tipPercentage/100
+  let tip = req.params.total * tipPercentile
+  res.send('Tip: ' + tip)
+})
+    
 
 app.listen(port,() => {
     console.log('listening on port' , port);
 });
-
-
-// Pick up work from Tip Calculator section.
-
